c52d6c08a86da2f7579ed9b92b8c769ca2704448
shuliu1314 <Shu.liu@vensi.com>
2022-11-08 00:20:48 -0600

    Remove sample project and only leave the sdk in the repo

--- scripts/setup-project.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-# prevent accidental commits on these files
-# need to undo, if changes to these files are actually needed
-git update-index --assume-unchanged samples/basic-sample/configs/app_config.h
-git update-index --assume-unchanged samples/basic-sample/configs/wifi_config.h
