58bc96bc85c58b44d2caf34fa06c19833e1ebc21
Nik Markovic <nikola.m@gmail.com>
2024-07-11 12:32:32 -0700

    Synchronous OTA rework (#8)
    * Synchronous OTA rework
    
    * Implement comments from online review with Shu
    
    ---------
    
    Co-authored-by: nik-markovic <nikola.markovic@avnet.com>

--- source/iotc_ota.c
+++ source/iotc_ota.c
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: MIT
  * Copyright (C) 2024 Avnet
- * Authors: Shu Liu <shu.liu@avnet.com> et al.
+ * Authors: Shu Liu <shu.liu@avnet.com>, Nik Markovic<nikola.markovic@avnet.com> et al.
  */
 
 
@@ -8,11 +8,8 @@
 // To avoid potential issues with older BSPs if OTA is not supported
 #ifdef IOTC_OTA_SUPPORT
 
-/* Middleware libraries */
-//#include "cy_retarget_io.h"
+#include "cyabs_rtos.h"
 #include "cy_log.h"
-
-/* OTA API */
 #include "cy_ota_api.h"
 #include "cy_ota_storage_api.h"
 
@@ -26,14 +23,18 @@
 #define APP_ID                              (0)
 #define HTTP_SERVER_PORT	443
 
-/*******************************************************************************
- * Global Variables
- ********************************************************************************/
-/* OTA context */
+
+
+// Captured task handle that will will resume in case of synchronous OTA
+static TaskHandle_t app_task_handle;
+
+
+// Captured last error received at the time of OTA session completion
+static cy_rslt_t last_session_result = CY_RSLT_OTA_ERROR_GENERAL; // assume a failure unless we get CY_OTA_STATE_OTA_COMPLETE
+
 static cy_ota_context_ptr ota_context;
 
-static cy_ota_storage_interface_t ota_interfaces =
-{
+static cy_ota_storage_interface_t ota_interfaces = {
    .ota_file_open            = cy_ota_storage_open,
    .ota_file_read            = cy_ota_storage_read,
    .ota_file_write           = cy_ota_storage_write,
@@ -41,6 +42,31 @@ static cy_ota_storage_interface_t ota_interfaces =
    .ota_file_verify          = cy_ota_storage_verify,
    .ota_file_validate        = cy_ota_storage_image_validate,
    .ota_file_get_app_info    = cy_ota_storage_get_app_info
+};
+
+static cy_ota_agent_params_t ota_agent_params = {
+    // .cb_func = usr_ota_cb ? usr_ota_cb : iotc_ota_callback,
+	.cb_func = NULL,
+    .cb_arg = &ota_context,
+    .reboot_upon_completion = 1, /* Reboot after completing OTA with success. */
+    .validate_after_reboot = 1,
+    .do_not_send_result = 1
+};
+
+static cy_ota_network_params_t ota_network_params = {
+    .http = {
+        .server = {
+            .host_name = NULL,
+            .port = HTTP_SERVER_PORT
+        },
+        .file = NULL,
+        .credentials = {
+            .root_ca = NULL,
+            .root_ca_size = 0,
+        },
+    },
+    .use_get_job_flow = CY_OTA_DIRECT_FLOW,
+    .initial_connection = CY_OTA_CONNECTION_HTTPS
 };
 
 /*******************************************************************************
@@ -178,6 +204,9 @@ static cy_ota_callback_results_t iotc_ota_callback(cy_ota_cb_struct_t *cb_data) 
 
 		case CY_OTA_STATE_OTA_COMPLETE:
 			printf("APP CB OTA Session Complete\n");
+			last_session_result = cy_ota_get_last_error();
+			xTaskNotifyGive(app_task_handle);
+			cb_result = CY_OTA_CB_RSLT_OTA_STOP;
 			break;
 
 		case CY_OTA_STATE_STORAGE_OPEN:
@@ -228,48 +257,6 @@ cy_rslt_t iotc_ota_storage_validated(void) {
 	return result;
 }
 
-static cy_ota_agent_params_t ota_agent_params =
-{
-    // .cb_func = usr_ota_cb ? usr_ota_cb : iotc_ota_callback,
-	.cb_func = NULL,
-    .cb_arg = &ota_context,
-    .reboot_upon_completion = 1, /* Reboot after completing OTA with success. */
-    .validate_after_reboot = 1,
-    .do_not_send_result = 1
-};
-
-static cy_ota_network_params_t ota_network_params = {
-    .http = {
-        .server = {
-            .host_name = NULL,
-            .port = HTTP_SERVER_PORT
-        },
-        .file = NULL,
-        .credentials = {
-            .root_ca = NULL,
-            .root_ca_size = 0,
-        },
-    },
-    .use_get_job_flow = CY_OTA_DIRECT_FLOW,
-    .initial_connection = CY_OTA_CONNECTION_HTTPS
-};
-
-static cy_rslt_t iotc_ota_stop(void) {
-	if (ota_network_params.http.file != NULL) {
-		const char* file = ota_network_params.http.file;
-		iotcl_free((char* )file);
-		ota_network_params.http.file = NULL;
-	}
-	if (ota_network_params.http.server.host_name != NULL) {
-		const char* host_name = ota_network_params.http.server.host_name;
-		iotcl_free((char* )host_name);
-		ota_network_params.http.server.host_name = NULL;
-	} else {
-		return -1;
-	}
-	return CY_RSLT_SUCCESS;
-}
-
 cy_rslt_t iotc_ota_start(IotConnectConnectionType connection_type, const char *host, const char *path, cy_ota_callback_t usr_ota_cb) {
 	if (path == NULL || host == NULL) {
 		return -1;
@@ -290,17 +277,69 @@ cy_rslt_t iotc_ota_start(IotConnectConnectionType connection_type, const char *h
 
 	}
 
+	last_session_result = CY_RSLT_OTA_ERROR_GENERAL; // assume a failure unless we get CY_OTA_STATE_OTA_COMPLETE
+	iotc_ota_cleanup();
+
 	ota_agent_params.cb_func = usr_ota_cb ? usr_ota_cb : iotc_ota_callback;
 
 	ota_network_params.http.file = iotcl_strdup(path);
 	ota_network_params.http.server.host_name = iotcl_strdup(host);
 
 	cy_rslt_t result = cy_ota_agent_start(&ota_network_params, &ota_agent_params, &ota_interfaces, &ota_context);
-
-	if (result != CY_RSLT_SUCCESS) {
+	if (CY_RSLT_SUCCESS != result) {
 		printf("Initializing and starting the OTA agent failed. Error is %lu\n", result);
-		iotc_ota_stop();
+		iotc_ota_cleanup();
 	}
 	return result;
 }
+
+cy_rslt_t iotc_ota_run(IotConnectConnectionType connection_type, const char *host, const char *path, cy_ota_callback_t usr_ota_cb) {
+	cy_rslt_t result = iotc_ota_start(connection_type, host, path, usr_ota_cb);
+	if (CY_RSLT_SUCCESS != result) {
+		// OTA did not start. The called function will print the error.
+		return result;
+	}
+	app_task_handle = xTaskGetCurrentTaskHandle();
+	// wait for OTA completion
+	ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(60 * 60 * 1000)); // one hour
+
+	iotc_ota_cleanup();
+
+	// the user should call
+	return result;
+}
+
+cy_rslt_t iotc_ota_get_download_status(void) {
+	return last_session_result;
+}
+
+const char* iotc_ota_get_download_error_string(void) {
+	if (CY_RSLT_SUCCESS != last_session_result) {
+		return cy_ota_get_error_string(last_session_result);
+	} else {
+		return NULL;
+	}
+}
+
+void iotc_ota_system_reset(void) {
+	// we don't have support for ThreadX, but there just in case we add it
+#ifdef COMPONENT_THREADX
+	cyhal_system_reset_device();
+#else
+	NVIC_SystemReset();
+#endif
+}
+
+void iotc_ota_cleanup() {
+	ota_agent_params.cb_func = NULL;
+	if (ota_network_params.http.file) {
+		iotcl_free((char *) ota_network_params.http.file);
+		ota_network_params.http.file = NULL;
+	}
+	if (ota_network_params.http.server.host_name) {
+		iotcl_free((char *) ota_network_params.http.server.host_name);
+		ota_network_params.http.server.host_name = NULL;
+	}
+}
+
 #endif // IOTC_OTA_SUPPORT
